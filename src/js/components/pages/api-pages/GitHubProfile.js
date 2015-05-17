b75d76ca3a2bb80147c7ca01ecfb0caba1013501
Sergey Voloshin <voloshin1986@gmail.com>
2015-05-17 20:47:56 +0300

    Add github and stackoverflow data on soc.activity page.

--- src/js/components/pages/api-pages/GitHubProfile.js
+++ src/js/components/pages/api-pages/GitHubProfile.js
@@ -1,13 +1,19 @@
 var React = require('react');
+var mui = require('material-ui');
+var Paper = mui.Paper;
+var $ = require('jquery');
 
 var GitHubProfile = React.createClass({
 
   _loadData: function() {
-    var url ="https://api.github.com/users/VoloshinS"
+    var url ="https://api.github.com/users/VoloshinS";
     $.ajax({
       url: url, 
       success: function(data){
-        this.setState({data: data})
+        this.setState({data: data});
+      }.bind(this),
+      error: function(data){
+        console.log('Data not recieved!')
       }.bind(this)
     });
   },
@@ -18,27 +24,34 @@ var GitHubProfile = React.createClass({
 
   getInitialState: function() {
     return {
-      data: [] 
+      data: {}
     };
   },
 
   render: function() {
-    
-    var data = JSON.parse(this.state.data);
-    var public_repos = data.public_repos;
-    var public_gists = data.public_gists;
-    var followers = data.followers;
-    var following = data.following;
-    var created_at = data.created_at;
+    var data = this.state.data;
+    var created_at = data.created_at || 0;
+    var public_repos = data.public_repos || 0;
+    var public_gists = data.public_gists || 0;
+    var followers = data.followers || 0;
+    var following = data.following || 0;
+
+    if (created_at) {
+      var formattedDate = new Date(Date.parse(created_at));
+      var d = formattedDate.getDate();
+      var m =  formattedDate.getMonth()+1;
+      var y = formattedDate.getFullYear();
+      created_at = d + "." + m + "." + y;
+    }
 
     return (
       <Paper>
-        <h2>GitHub Data:</h2>
-        <p>Account created: {public_repos}</p>
-        <p>Public repos: {public_repos}</p>
-        <p>Public gists: {public_gists}</p>
-        <p>Followers: {followers}</p>
-        <p>Following: {following}</p>
+        <h4>GitHub Data:</h4>
+        Account created: {created_at}<br/>
+        Public repos: {public_repos}<br/>
+        Public gists: {public_gists}<br/>
+        Followers: {followers}<br/>
+        Following: {following}<br/>
       </Paper>
     );
   }
