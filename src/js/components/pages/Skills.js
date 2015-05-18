0e9f16029c09c35dc526fd65b8b87ff3a65069a3
Sergey Voloshin <sv@anahoret.com>
2015-05-18 15:23:10 +0300

    Add some skills.

--- src/js/components/pages/Skills.js
+++ src/js/components/pages/Skills.js
@@ -7,6 +7,16 @@ var Skills = React.createClass({
     return (
       <Paper innerClassName='vs-page'>
         <h1>My skills</h1>
+        <h3>HTML</h3>
+        <div>HTML4, HTML5, erb, haml, mustache, jade</div>
+        <h3>CSS</h3>
+        <div>CSS2, CSS3, sass, less, bootstrap, grids</div>
+        <h3>JavaScript</h3>
+        <div>ECMAScript 5, coffeescript, jquery, underscore, node, npm, react, gulp, grunt</div>
+        <h3>Ruby</h3>
+        <div>Ruby 2.0, Rails 4.0</div>
+        <h3>Database</h3>
+        <div>SQL, PostgreSQL</div>
       </Paper>
     );
   }
