cbe8b19efed80d8e041d964ba2d8be6b69a0f41d
Sergey Voloshin <sv@anahoret.com>
2016-06-02 16:58:15 +0300

    Configure release.

--- scripts/release.sh
+++ scripts/release.sh
@@ -10,12 +10,12 @@ npm run build
 git config user.name || git config user.name "Travis CI"
 git config user.email || git config user.email "travis@voloshins.js.org"
 # 3. Remove dist/ folder from .gitignore
-sed -i.bak '/\/dist\//d' .gitignore
+sed -i.bak '/build/d' .gitignore
 # 4. Add release files to new branch
 git branch | grep releases && git branch -D releases
 git checkout -b releases
 git add --all
 git commit -m "Released after: ${SHA}"
-git push origin releases
+# git push origin releases -f
 
 # 5. After that travis will automatically deploy from releases branch to deis repo.
